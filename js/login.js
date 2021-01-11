6e5798ef7561bf83317d4c0b80c40ff9342bfc2f
chanshukei <35111723+chanshukei@users.noreply.github.com>
2021-01-11 11:17:07 +0000

    css and js
    css and js

---
+++ js/login.js
@@ -0,0 +1,51 @@
+// JavaScript source code
+function signIn() {
+    console.log('called signIn');
+    var pData = {
+        "action": 'SIGNIN',
+        "usernameEmail": $('#usernameEmail').val(),
+        "password": $('#password').val()
+    };
+    $.ajax({
+        type: "POST",
+        url: "https://bizdoc.azurewebsites.net/api/SignInSession",
+        data: JSON.stringify(pData),
+        contentType: "application/json",
+        dataType: "json",
+        complete: function (jqXHR) {
+            if (jqXHR.readyState === 4) {
+                var signInSession = JSON.parse(jqXHR.responseText);
+                Cookies.set('sessionID', signInSession.signInSessionID);
+                window.location.href = '/project.html';
+            }
+        }
+    });
+
+}
+
+//google login
+function onGoogleSignInSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getEmail);
+}
+function onGoogleSignInFailure(error) {
+    console.log(error);
+}
+function renderButton() {
+    gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': 340,
+        'height': 50,
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onGoogleSignInSuccess,
+        'onfailure': onGoogleSignInFailure
+    });
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
---
+++ js/sitemap.js
@@ -0,0 +1,4 @@
+// JavaScript source code
+function gotoMeetingMinute() {
+    window.location.href = '/meetingMinutes.html';
+}
