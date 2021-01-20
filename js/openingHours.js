d355b6e8b4c427fae14cbb0b78168030b2df0e7d
chanshukei <35111723+chanshukei@users.noreply.github.com>
2021-01-20 12:01:19 +0000

    add 3 steps

--- js/openingHours.js
+++ js/openingHours.js
@@ -3,7 +3,17 @@ function goStep2(){
     $('#step2').show();
 }
 
+function goStep3(){
+    $('#step2').hide();    
+    $('#step3').show();
+}
+
 function backStep1(){
     $('#step1').hide();    
+    $('#step2').show();
+}
+
+function backStep2(){
+    $('#step3').hide();    
     $('#step2').show();
 }
