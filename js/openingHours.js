fa2423fd1d0caed182227095821038102720bef5
chanshukei <35111723+chanshukei@users.noreply.github.com>
2021-01-22 00:00:37 +0000

    update open hour

--- js/openingHours.js
+++ js/openingHours.js
@@ -3,25 +3,6 @@ function goStep2(){
     $('#step2').show();
 }
 
-function goStep3(){
-    $('#step2').hide();
-    $("#monOpenTime").val($("#openTime").val());
-    $("#monCloseTime").val($("#closeTime").val());
-    $("#tueOpenTime").val($("#openTime").val());
-    $("#tueCloseTime").val($("#closeTime").val());
-    $("#wedOpenTime").val($("#openTime").val());
-    $("#wedCloseTime").val($("#closeTime").val());
-    $("#thuOpenTime").val($("#openTime").val());
-    $("#thuCloseTime").val($("#closeTime").val());
-    $("#friOpenTime").val($("#openTime").val());
-    $("#friCloseTime").val($("#closeTime").val());
-    $("#satOpenTime").val($("#openTime").val());
-    $("#satCloseTime").val($("#closeTime").val());
-    $("#sunOpenTime").val($("#openTime").val());
-    $("#sunCloseTime").val($("#closeTime").val());
-    $('#step3').show();
-}
-
 function backStep1(){
     $('#step2').hide();    
     $('#step1').show();
